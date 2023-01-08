a90bfd8e325d03d49e36fa3e3c67e0584c8c69fa
Muhammad Haikal <mhaikalalichaidar@gmail.com>
2023-01-09 05:45:21 +0700

    website kawaskaki

--- application/views/login.php
+++ application/views/login.php
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="index.php"><b>Kawas </b></a>
+            <a href="index.php"><b>Kawas Kaki</b></a>
         </div>
         <!-- /.login-logo -->
         <div class="login-box-body">
